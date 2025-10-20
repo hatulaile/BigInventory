809bc28243f24ca8d352d1d8d3b7f9cc5d23c93b
hatulaile <hatulaile@outlook.com>
2025-10-20 17:12:43 +0800

    feat: 添加功能

---
+++ BigInventory/ModBehaviour.cs
@@ -0,0 +1,27 @@
+using HarmonyLib;
+
+namespace BigInventory
+{
+    public class ModBehaviour : Duckov.Modding.ModBehaviour
+    {
+        public const float INVENTORY_CAPACITY_MULTIPLIER = 2f;
+        public const float INVENTORY_WEIGHT_MULTIPLIER = 1f;
+        public const int PLAYER_STORAGE_MULTIPLIER = 3;
+
+
+        private Harmony? _harmony;
+
+        protected override void OnAfterSetup()
+        {
+            base.OnAfterSetup();
+            _harmony = new Harmony("duckov_big_inventory_hatu");
+            _harmony.PatchAll();
+        }
+
+        protected override void OnBeforeDeactivate()
+        {
+            base.OnBeforeDeactivate();
+            _harmony?.UnpatchAll();
+        }
+    }
+}
---
+++ BigInventory/Patches/CharacterMainControlPatches.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using HarmonyLib;
+
+namespace BigInventory.Patches;
+
+[SuppressMessage("ReSharper", "InconsistentNaming")]
+[HarmonyPatch(typeof(CharacterMainControl))]
+public class CharacterMainControlPatches
+{
+    [HarmonyPostfix]
+    [HarmonyPatch(nameof(CharacterMainControl.InventoryCapacity), MethodType.Getter)]
+    public static void InventoryCapacityGetterPostfix(CharacterMainControl __instance,ref float __result)
+    {
+        __result *= ModBehaviour.INVENTORY_CAPACITY_MULTIPLIER;
+    }
+
+    [HarmonyPostfix]
+    [HarmonyPatch(nameof(CharacterMainControl.MaxWeight), MethodType.Getter)]
+    public static void MaxWeightGetterPostfix(ref float __result)
+    {
+        __result *= ModBehaviour.INVENTORY_WEIGHT_MULTIPLIER;
+    }
+}
